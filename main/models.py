73f522cdc623909fd2cd48fdb497dbb82e55fd5e
callistaputri <callistaashf07@gmail.com>
2026-09-13 15:22:15 +0700

    Menambahkan fitur Education pada portofolio

--- main/models.py
+++ main/models.py
@@ -24,3 +24,13 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    field_of_study = models.CharField(max_length=255, blank=True)
+    started = models.DateField()
+    ended = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.institution
--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
-
+from main.models import Experience, Education
 
 def show_main(request):
     context = {
@@ -25,3 +24,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Callista Putri Anjola",
+        "education_list": Education.objects.all()
+    }
+    return render(request, "education.html", context)
